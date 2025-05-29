cb70aeb4a6ca8d3e5ee2d05461fb03ff49f5c7b3
adantuon <adantuon@mail.umw.edu>
2025-05-29 18:49:45 -0400

    Modified size of erase_bullet

--- Moving Custom Sprite/bullet.cpp
+++ Moving Custom Sprite/bullet.cpp
@@ -21,7 +21,7 @@ void bullet::fire()
 }
 void bullet::erase_bullet()
 {
-	al_draw_filled_rectangle(x,y,x+5,y+5,al_map_rgb(0,0,0)); //black color
+	al_draw_filled_rectangle(x,y,x+16,y+16,al_map_rgb(0,0,0)); //black color
 }
 int bullet::move_bullet (int arrowX, int arrowY, int width, int length, int height)
 {
